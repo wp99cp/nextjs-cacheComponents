6d09814f07ca5b5e9d3bb610863451846761c131
Cyrill Püntener <wp99cp@gmx.net>
2025-10-28 15:52:58 +0100

    add landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+
+const Page: React.FC = () => {
+
+    return <>
+        <h1>Welcome to the Demo Page</h1>
+        <p>This page is rendered statically at build time.</p>
+
+        <h2>Visit some dynamic pages</h2>
+        - <Link href='/public'>A public page (pre-rendered)</Link>
+        - <Link href='/public-on-visit'>A public page (rendered on first visit, then cached)</Link>
+        <br/>
+        - <Link href='/private'>A private page</Link>
+
+    </>
+}
+
+
+export default Page
